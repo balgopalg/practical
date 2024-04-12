2166bfe21758c32f3d6c314dd9c25eb8a85d7061
G. Bal Gopal <71633683+balgopalg@users.noreply.github.com>
2024-04-12 18:00:23 +0530

    Update 02_mulTable.r

--- ds/02_mulTable.r
+++ ds/02_mulTable.r
@@ -1,3 +1,7 @@
+2.	Write a program that prints a multiplication table for numbers up to 12. 
+Program:-
+
+
 num = as.integer(readline(prompt = "Enter a number: "))
 for(i in 1:12) {
 print(paste(num,'x', i, '=', num*i))
